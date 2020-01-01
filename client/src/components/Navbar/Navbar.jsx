4c2a404acb307ff4379663b5e4591cba15839943
SiddharthaSauravGogoi <lezaroth3@gmail.com>
2020-01-01 10:44:48 +0530

    Initialized React Router. Landing page nav added.

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+    return(
+        <div className="landing-page-navbar">
+            <h3>Chat App</h3>
+            <ul>
+                <li>Login</li>
+                <li>Sign Up</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
